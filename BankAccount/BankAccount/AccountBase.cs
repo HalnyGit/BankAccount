16b6aac4d8b74a9a418dc29b0dcb77399325c6ec
HalnyGit <haliniakm@gmail.com>
2023-02-28 19:33:05 +0100

    Poprawka

--- BankAccount/BankAccount/AccountBase.cs
+++ BankAccount/BankAccount/AccountBase.cs
@@ -8,18 +8,12 @@
         }
 
         public string AccountID { get; private set; }
-
         public delegate void CashflowAddedDelegate(object sender, EventArgs args);
         public abstract event CashflowAddedDelegate CashflowAdded;
-
         public abstract void AddCashflow(float cashflow);
-
         public abstract void AddCashflow(string cashflow);
-
         public abstract void AddCashflow(double grade);
-
         public abstract void AddCashflow(decimal grade);
-
         public abstract Statistics GetStatistics();
     }
 }
--- BankAccount/BankAccount/IAccount.cs
+++ BankAccount/BankAccount/IAccount.cs
@@ -4,16 +4,11 @@ namespace BankAccount
     public interface IAccount
     {
         string AccountID { get; }
-
         void AddCashflow(float cashflow);
         void AddCashflow(string cashflow);
-
         void AddCashflow(double grade);
-
         void AddCashflow(decimal grade);
-
         event CashflowAddedDelegate CashflowAdded;
-
         Statistics GetStatistics();
     }
 }
